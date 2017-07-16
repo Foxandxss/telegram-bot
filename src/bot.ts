10b72c294a4d775d57255f137550bd7c3bb2e339
Jesus Rodriguez <Foxandxss@gmail.com>
2017-07-16 22:29:01 +0200

    extract plugin registration to another file

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
-import { RedditPlugin } from './Plugins/reddit';
-import { Plugin } from './Plugins/plugin.interface';
+
+import { availablePlugins } from "./pluginsRegistry";
 
 const config = require('../config.json');
 
@@ -9,23 +9,13 @@ if (!config.token) {
   process.exit(1);
 }
 
-let plugins: Plugin[] = [];
-
-config.plugins.forEach(plugin => {
-  switch (plugin.name) {
-    case 'reddit':
-      plugins.push(new RedditPlugin(plugin.command, plugin.subreddits, plugin.limit || 10));
-      break;
-  }
-});
-
 const bot = new TelegramBot(config.token, {polling: true});
 
 bot.on('message', (msg) => {
   try {
     if (!msg.text || msg.text.toLowerCase().indexOf('!') !== 0) return;
     
-    const usablePlugins = plugins.filter(p => p.command === msg.text.toLowerCase().substr(1));
+    const usablePlugins = availablePlugins.filter(p => p.command === msg.text.toLowerCase().substr(1));
     usablePlugins.forEach(up => {
       up.exec(bot, msg);
     });
---
+++ src/pluginsRegistry.ts
@@ -0,0 +1,14 @@
+import { RedditPlugin } from './Plugins/reddit';
+import { Plugin } from './Plugins/plugin.interface';
+
+const config = require('../config.json');
+
+export let availablePlugins: Plugin[] = [];
+
+config.plugins.forEach(plugin => {
+  switch (plugin.name) {
+    case 'reddit':
+      availablePlugins.push(new RedditPlugin(plugin.command, plugin.subreddits, plugin.limit || 10));
+      break;
+  }
+});
